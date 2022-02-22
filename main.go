599919cf0f809af70de6bc101c546ccd8ad88a3d
Justin Onstot <justin.onstot@koddi.com>
2022-02-22 09:43:56 -0800

    Commiting changes to my repo for Eliza example.

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"myapp/doctor"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	whatToSay := doctor.Intro()
+
+	fmt.Println(whatToSay)
+
+	// Only one kind of loop in GO
+	for {
+
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+
+	}
+}
