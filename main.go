45f7ea60f6abdbbfbc175e6aba5575dd10b388f7
jmokoddi <justin.onstot@koddi.com>
2022-07-01 10:22:31 -0700

    changes

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	// "myapp/doctor"
+)
+
+func main() {
+
+	whatToSay := "Hello there!"
+	//whatToSay = doctor.Intro()
+
+	fmt.Println(whatToSay)
+}
--- scope/packageone/package.go
+++
@@ -1,9 +0,0 @@
-package packageone
-
-import "fmt"
-
-var PackageVar string = "I am PackageVar"
-
-func PrintMe(s1, s2 string) {
-	fmt.Println(s1, s2, PackageVar)
-}
