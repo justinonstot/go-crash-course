289792cd6b1e7430e76807064da9fafb4b6c1e67
jmokoddi <justin.onstot@koddi.com>
2022-07-02 15:27:53 -0700

    doctor go stuff

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	// "myapp/doctor"
+	"myapp/doctor"
+	"os"
+	"strings"
 )
 
 func main() {
 
-	whatToSay := "Hello there!"
-	//whatToSay = doctor.Intro()
+	reader := bufio.NewReader(os.Stdin)
+
+	whatToSay := doctor.Intro()
 
 	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n') // a rune. Capture until return char and store
+
+		userInput = strings.Replace(userInput, "\r\n", "", -1) // for windows CRLF
+		userInput = strings.Replace(userInput, "\n", "", -1)   // for mac/linux
+
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+	}
+
 }
